5cf8c2aea437c345724b48f25135c95de7d4bf1a
Steven Kang <skang28@gmail.com>
2019-11-26 21:31:41 -0500

    chapter 14 assignment

---
+++ src/shopping-list-service.js
@@ -0,0 +1,28 @@
+const ShoppingService = {
+    getAllItems(knex) {
+        return knex.select('*').from('shopping_list')
+    },
+
+    insertItem(knex, newItem) {
+        return knex
+            .insert(newItem)
+            .into('shopping_list')
+            .returning('*')
+            .then(rows => {
+                return rows[0]
+            })
+    },
+
+    deleteItem(knex, id) {
+        return knex('shopping_list')
+            .where({id})
+            .delete()
+    },
+    updateItem(knex, id, newItemFields) {
+        return knex('shopping_list')
+            .where({id})
+            .update(newItemFields)
+    }
+}
+
+module.exports = ShoppingService
